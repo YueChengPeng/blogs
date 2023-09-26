fd4ed74012ca411d55b18b13029f2c9b05ad7463
YueChengPeng <75080955+YueChengPeng@users.noreply.github.com>
2023-09-26 16:29:57 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     name: "Yuecheng Peng",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "fabrication researcher",
-    bio: "I fabricate everything with hands.",
+    bio: "Fabricate Everything.",
     email: "yuechengpengcoco@gmail.com",
     linkedin: "",
     github: "YueChengPeng",
